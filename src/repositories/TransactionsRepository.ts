fca556a38cce856c549c2176f595408b01b636f4
Ronaldo Azarias <ronaldo.azarias01@gmail.com>
2020-09-24 09:49:02 -0300

    Comentário

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,10 +8,51 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
+
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find(); //Devido estar no repositório, já busca no BD
+    const { income, outcome } = transactions.reduce((acc: Balance, transaction: Transaction) => {
+      switch(transaction.type) {
+        case "income":
+          acc.income += Number(transaction.value);
+          break;
+        case "outcome":
+          acc.outcome += Number(transaction.value);
+          break;
+      }
+      return acc;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    /*const { income, outcome } = transactions.reduce((acc, transaction) => {
+      switch(transaction.type) {
+        case "income":
+          acc.income += Number(transaction.value);
+          break;
+        case "outcome":
+          acc.outcome += Number(transaction.value);
+          break;
+      }
+      return acc;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });    */
+  
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
